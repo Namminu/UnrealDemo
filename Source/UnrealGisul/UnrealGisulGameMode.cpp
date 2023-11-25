37b7b19b40646a33a962fd108cab5c3f438cb128
rootinge <jhpark5129@gmail.com>
2023-11-25 23:49:05 +0900

    애니메이션 추가
    애니메이션 추가
    다음에 할것
    애니메이션 공격 추가, 밑에 그림자 따라오는거 수정, 손에 무기들기

--- Source/UnrealGisul/UnrealGisulGameMode.cpp
+++ Source/UnrealGisul/UnrealGisulGameMode.cpp
@@ -7,7 +7,7 @@
 AUnrealGisulGameMode::AUnrealGisulGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/WizardSM_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
