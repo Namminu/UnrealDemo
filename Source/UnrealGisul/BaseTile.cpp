fd9d52c971b88083c5a73633a5813072a3f83385
20191143namminwoo <mvpalsdn771@gmail.com>
2023-12-02 22:36:18 +0900

    타일 오브젝트

--- Source/UnrealGisul/BaseTile.cpp
+++ Source/UnrealGisul/BaseTile.cpp
@@ -2,6 +2,9 @@
 
 
 #include "BaseTile.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Components/BoxComponent.h"
+#include "TimerManager.h"
 
 // Sets default values
 ABaseTile::ABaseTile()
@@ -9,6 +12,29 @@ ABaseTile::ABaseTile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//StaticMesh
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TileMesh"));
+	Body->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+	//Box Collision
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	CollisionBox->AttachToComponent(Body, FAttachmentTransformRules::KeepRelativeTransform);
+	//Box Collision OverlapEvent Bind
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ABaseTile::BeginOverlap);
+
+	//Particle
+	ParticleComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleComponent"));
+	ParticleComponent->AttachToComponent(Body, FAttachmentTransformRules::KeepRelativeTransform);
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleSystemAsset(TEXT("/Game/StarterContent/Particles/P_Sparks"));
+	if (ParticleSystemAsset.Succeeded())
+	{
+		ParticleComponent->SetTemplate(ParticleSystemAsset.Object);
+		//ParticleComponent->ActivateSystem();
+	}
+
+	//변수 초기화
+	isTurn = false;
+	delayTime = 5.0;
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +48,40 @@ void ABaseTile::BeginPlay()
 void ABaseTile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	EffectOnOff_Implementation();
 }
 
+//변수값에 따라 이펙트 on/off 함수
+void ABaseTile::EffectOnOff_Implementation()
+{
+	if(isTurn == true)
+	{
+		ParticleComponent->SetVisibility(true);
+		ParticleComponent->SetComponentTickEnabled(true);
+		//delayTime 만큼 딜레이 후
+		FTimerHandle TimerHandle;
+		FTimerDelegate TimerDelegate;
+		TimerDelegate.BindUFunction(this, FName("DelayBool"));
+		GetWorldTimerManager().SetTimer(TimerHandle, TimerDelegate, delayTime, false);
+	}
+	else if(isTurn == false)
+	{
+		ParticleComponent->SetVisibility(false);
+		ParticleComponent->SetComponentTickEnabled(false);	
+	}
+}
+
+//박스 콜리전 오버랩 이벤트
+void ABaseTile::BeginOverlap_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	isTurn = true;
+
+	UE_LOG(LogTemp, Warning, TEXT("Overlap"));
+}
+
+//딜레이 후 일어나는 이벤트
+void ABaseTile::DelayBool()
+{
+	isTurn = false;
+}
--- Source/UnrealGisul/BaseTile.h
+++ Source/UnrealGisul/BaseTile.h
@@ -10,10 +10,48 @@ UCLASS()
 class UNREALGISUL_API ABaseTile : public AActor
 {
 	GENERATED_BODY()
-	
+
+private:
+	//전체 타일의 순서를 정할 변수
+	UPROPERTY(EditAnywhere, Category = "Tile Count", meta = (AllowPrivateAccess = "true"))
+	int myNum;
+
+	//타일이 플레이어에게 밟혔는지 체크하기 위한 bool 변수
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Tile Count", meta = (AllowPrivateAccess = "true"))
+	bool isTurn;
+
+	//스테틱 메시
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* Body;
+
+	//isTUrn 이 true일 때의 연출을 위한 파티클 컴포넌트
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Tile Count", meta = (AllowPrivateAccess = "true"))
+	UParticleSystemComponent* ParticleComponent;
+
+	//플레이어와의 오버랩을 검사할 콜리전 박스
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Tile Count", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* CollisionBox;
+
+	//딜레이 시간
+	UPROPERTY(EditAnywhere, Category = "Effect Event", meta = (AllowPrivateAccess = "true"))
+	float delayTime;
+
 public:	
 	// Sets default values for this actor's properties
 	ABaseTile();
+
+	//파티클 이펙트를 조절하는 함수
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Tile Count", meta = (AllowPrivateAccess = "true"))
+	void EffectOnOff();
+
+	//오버랩 이벤트
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Effect Event", meta = (AllowPrivateAccess = "true"))
+	void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	//딜레이 함수 - isTurn 의 값을 바꾸기 위함
+	UFUNCTION()
+	void DelayBool();
 
 protected:
 	// Called when the game starts or when spawned
